5c0be742493f6b9335908df370665a8b6adcb142
rxdrigoha <rodrigo.a.pratesprimary@gmail.com>
2025-05-21 14:45:38 -0300

    teste

---
+++ aula003/app.js
@@ -0,0 +1,10 @@
+let titulo = document.querySelector('h1')
+titulo.innerHTML = 'Jogo do número secreto';
+
+let paragrafo = document.querySelector('p')
+
+paragrafo.innerHTML = 'Escolha um número de 1 a 10';
+
+function verificarChute() {
+    console.log('O botao foi clicado')   
+}
