837464b7cc80b58ba07bd0b2fa4ef3d4244f5057
rxdrigoha <rodrigo.a.pratesprimary@gmail.com>
2025-05-21 18:10:52 -0300

    gg

--- js-curso-2-aula_2/app.js
+++ js-curso-2-aula_2/app.js
@@ -6,15 +6,25 @@ function exibirTextoNaTela(tag, texto) {
 }
 
 exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 1000');
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
-    console.log(chute == numeroSecreto);
+    
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Voce acertou');
+        exibirTextoNaTela('p', 'O número secreto era: ' + numeroSecreto);
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor que ' + chute);
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior que ' + chute);
+        }
+    }
 }
 
 function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 10 + 1);
+    return parseInt(Math.random() * 1000 + 1);
 }
 
 
