87fc6b5a07a832f97bc590376127f85c229c8682
rxdrigoha <rodrigo.a.pratesprimary@gmail.com>
2025-05-21 16:41:34 -0300

    z

--- aula003/app.js
+++ aula003/app.js
@@ -1,12 +1,22 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Jogo do número secreto';
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número de 1 a 10';
+// let paragrafo = document.querySelector('p');
+// paragrafo.innerHTML = 'Escolha um número de 1 a 10';
 
-let campo = document.querySelector(tag)
-campo.innerHTML = texto
+let numeroSecreto = gerarNumeroAleatorio();
+
+function exibirTexto(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+exibirTexto('h1', 'Jogo do número secreto');
+exibirTexto('p', 'Escolha um número de 1 a 1000');
 
 function verificarChute() {
-    console.log('O botao foi clicado') 
+    let chute = document.querySelector('input').value;
+    console.log(chute ==    numeroSecreto);
+}
+function gerarNumeroAleatorio() {
+   return parseInt(Math.random() * 1000 + 1); // Errei: "math" Consertei: "Math"
 }
