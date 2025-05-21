1301c7ec24f4869c3ec2061b27b303877e9e8613
rxdrigoha <rodrigo.a.pratesprimary@gmail.com>
2025-05-21 15:25:04 -0300

    z

---
+++ js-curso-2-desafio_1/app.js
@@ -0,0 +1,10 @@
+let titulo = document.querySelector('h1')
+titulo.innerHTML='Hora do Desafio'
+
+function verificarClick() {
+    console.log('O botao foi clicado')
+}
+
+function iniciarAlerta() {
+    console.log('Eu amo JS')
+}
