ea708195b25cbcf5d3a33165979d0c91989def35
rxdrigoha <rodrigo.a.pratesprimary@gmail.com>
2025-05-21 17:53:23 -0300

    aeaewe

--- js-curso-2-aula_2/app.js
+++ js-curso-2-aula_2/app.js
@@ -6,14 +6,14 @@ function exibirTextoNaTela(tag, texto) {
 }
 
 exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 1000');
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
 
     if (chute == numeroSecreto) {
-        exibirTextoNaTela('h2', 'Você acertou, parabéns.'); //Errei: Esqueci de botar o T em maiusculo //
-    } else {
+        exibirTextoNaTela('h1', 'Você acertou, parabéns.'); //Errei: Esqueci de botar o T em maiusculo //
+    } else {        
         if (chute < numeroSecreto) {
             exibirTextoNaTela('h2', 'O número secreto é maior.');
         } else {
