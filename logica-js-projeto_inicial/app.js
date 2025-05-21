f31b4f14f2a8efc5262a5549187b512114b23281
rxdrigoha <rodrigo.a.pratesprimary@gmail.com>
2025-05-21 17:16:11 -0300

    xzzzzx

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,9 +1,23 @@
 console.log('Arquivo app.js carregado!');
-    
+
 function exibirOla() {
     console.log('Olá, mundo!');
   }
   function exibirNome() {
     let nome = prompt('Qual é o seu nome?');
     console.log(`Olá, ${nome}!`);   
+  }
+
+  function exibirDobro() {
+    let numeroI = parseInt(prompt('Digite um número:'));
+    let resultado = numeroI * 2;
+    alert(`O dobro de ${numeroI} é ${resultado}`);
+  }
+
+  function exibirMedia() {
+    let numero1 = parseInt(prompt('Digite o primeiro número:'));
+    let numero2 = parseInt(prompt('Digite o segundo número:'));
+    let numero3 = parseInt(prompt('Digite o terceiro número:'));
+    let resultado = (numero1 + numero2 + numero3) / 3;
+    alert(`A média de ${numero1}, ${numero2} e ${numero3} é ${resultado}`);
   }
