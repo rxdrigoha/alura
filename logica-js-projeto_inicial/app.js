7a32b56467778570362ba557a659a43ef7be69f1
rxdrigoha <rodrigo.a.pratesprimary@gmail.com>
2025-05-21 16:53:47 -0300

    gg

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,10 @@
+function exibirOla() {
+    console.log("Olá, mundo!");
+  }
+  
+  exibirOla();
+
+  function exiberNome() {
+    let nome = prompt('Qual é o seu nome?');
+    console.log(`Olá, ${nome}!`);   
+  }
