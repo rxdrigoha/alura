27ae02bd2327a92683ec3fab84e3762cccf82d99
rxdrigoha <rodrigo.a.pratesprimary@gmail.com>
2025-05-28 14:48:14 -0300

    Update app.js

---
+++ 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -0,0 +1,9 @@
+function sortear() {
+    let quantidade = document.getElementById("quantidade").value;
+    let de = document.getElementById("de").value;
+    let ate = document.getElementById("ate").value;
+
+    alert (`Quantiade: ${quantidade}`)
+    alert (`Do numero: ${de}`)
+    alert (`Ate o numero: ${ate}`)
+}
