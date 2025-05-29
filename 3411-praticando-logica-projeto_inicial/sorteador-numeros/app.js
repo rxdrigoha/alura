ebc981c1d13af5f08e695a6e53b5c395153fef12
rxdrigoha <rodrigo.a.pratesprimary@gmail.com>
2025-05-29 15:35:08 -0300

    gg

--- 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
+++ 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -2,17 +2,23 @@ function sortear() {
     let quantidade = parseInt(document.getElementById("quantidade").value);
     let de = parseInt(document.getElementById("de").value);
     let ate = parseInt(document.getElementById("ate").value);
-    alerta();
+    if (!alerta(de, ate)) {
+        return; // Interrompe a execução se a validação falhar
+    }
+    if (!validarQuantidade(quantidade, de, ate)) {
+        return; // Interrompe a execução se a validação falhar
+    }
 
     let listaDeNumerosSorteados = [];
     let numero;
-    
+
     for (let i = 0; i < quantidade; i++) {
         numero = obterNumeroAleatorio(de, ate);
-        while (listaDeNumerosSorteados.includes(numero))
-            numero = obterNumeroAleatorio(de, ate); 
+        while (listaDeNumerosSorteados.includes(numero)) {
+            numero = obterNumeroAleatorio(de, ate);
+            alert('Tentando obter número inédito');
+        }
         listaDeNumerosSorteados.push(numero);
-
     }
 
 let resultado = document.getElementById("resultado");
@@ -46,12 +52,17 @@ function reiniciar() {
     alterarStatusBotao();
 }
 
-function alerta() {
+function alerta(de, ate) {
     let de = parseInt(document.getElementById("de").value);
     let ate = parseInt(document.getElementById("ate").value);
     if (de >= ate) {
-        alert('Preencha todos os campos de modo certo')
-        return;
-        
+        alert('Preencha todos os campos de modo certo');
+        return false;
     }
+    return true;
+}
+
+function validarQuantidade(quantidade, de, ate) {
+    let intervalo = ate - de + 1;
+    if (quantidade > intervalo) {alert('Quantidade de Numeros Sorteados inválida'); return false;} return true;
 }
