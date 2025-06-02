ff775a659c6fb3314c01441a506e073153921b83
rxdrigoha <rodrigo.a.pratesprimary@gmail.com>
2025-06-02 13:09:00 -0300

    FINISH

--- 3411-praticando-logica-projeto_inicial/amigo-secreto/js/app.js
+++ 3411-praticando-logica-projeto_inicial/amigo-secreto/js/app.js
@@ -2,6 +2,14 @@ let amigos = [];
 
 function adicionar() {
 let amigo = document.getElementById('nome-amigo');
+
+if (amigos.includes(amigo.value)) {
+    alert('Nome ja existe na lista!');
+    return;
+}
+
+
+
 let listaAmigos = document.getElementById('lista-amigos');
 amigos.push(amigo.value);
 if (listaAmigos.textContent == '') {
@@ -13,6 +21,12 @@ amigo.value = '';
 }
 
 function sortear() {
+    if (amigos.length < 4) {
+        alert('É necessário pelo menos 4 amigos para realizar o sorteio.');
+        return;
+    }
+
+
     embaralhar(amigos);
     let sorteio = document.getElementById('lista-sorteio');
     for (let i = 0; i < amigos.length; i++) {
@@ -26,4 +40,10 @@ function embaralhar(amigos) {
         let j = Math.floor(Math.random() * (i + 1));
         [amigos[i], amigos[j]] = [amigos[j], amigos[i]];
     }
+}
+
+function reiniciar() {
+    amigos = [];
+    document.getElementById('lista-amigos').textContent = '';
+    document.getElementById('lista-sorteio').innerHTML = '';                     
 }
