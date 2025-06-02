27ef2c2a0b4d33dc83b78be33a359562fa8885b0
rxdrigoha <rodrigo.a.pratesprimary@gmail.com>
2025-06-02 11:30:53 -0300

    amigo secreto

---
+++ 3411-praticando-logica-projeto_inicial/amigo-secreto/js/app.js
@@ -0,0 +1,29 @@
+let amigos = [];
+
+function adicionar() {
+let amigo = document.getElementById('nome-amigo');
+let listaAmigos = document.getElementById('lista-amigos');
+amigos.push(amigo.value);
+if (listaAmigos.textContent == '') {
+    listaAmigos.textContent = amigo.value;
+} else {
+    listaAmigos.textContent = listaAmigos.textContent + ', ' + amigo.value;
+}
+amigo.value = '';
+}
+
+function sortear() {
+    embaralhar(amigos);
+    let sorteio = document.getElementById('lista-sorteio');
+    for (let i = 0; i < amigos.length; i++) {
+        let proximo = (i + 1) % amigos.length;
+        sorteio.innerHTML += amigos[i] + ' --> ' + amigos[proximo] + '<br>';
+    }
+}
+
+function embaralhar(amigos) {
+    for (let i = amigos.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [amigos[i], amigos[j]] = [amigos[j], amigos[i]];
+    }
+}
